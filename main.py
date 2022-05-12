f97378eb59f98d11a120f8044c60f0706ffdde2f
matej2 <11059438+matej2@users.noreply.github.com>
2022-05-12 10:09:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ settings.configure()
 from dr_scaffold.generators import Generator
 from schemaorg.main import Schema
 
-origin_schema_name = 'MonetaryGrant'
+origin_schema_name = 'Mountain'
 
 TYPE_MAPPINGS = {
     'Text': 'CharField',
@@ -16,11 +16,11 @@ TYPE_MAPPINGS = {
     'Integer': 'IntegerField',
     'URL': 'URLField',
     'DateTime': 'DateTimeField',
+    'Time': 'TimeField',
     'Date': 'DateTimeField',
     'Boolean': 'BooleanField',
     'Place': 'CharField',
     'Brand': 'CharField',
-    'ImageObject': 'ImageField',
     'Thing': 'CharField',
     'Action': 'CharField'
 }
@@ -40,25 +40,35 @@ def url_to_entity(url):
     return re.sub(r'\W', '', ent, flags=re.IGNORECASE)
 
 
+def is_substring_in_list(sub, list):
+    for str in list:
+        if sub in str:
+            return True
+
+
 def generate_model(schema_name, lvl=1):
     schema = Schema(schema_name)
     toadd = []
-    toadd_line = []
     items = schema._properties.items()
     # TODO: Replace with recursion
     # Get sub objects first
     for name, meta in items:
         entity = url_to_entity(meta['rangeIncludes'])
         if entity not in TYPE_MAPPINGS and lvl > 0:
-            print(f'Go into {name}:{entity}, lvl {lvl}')
+            print(f'{entity} -> {name}, lvl {lvl}')
             if entity not in generated:
-                generated.append(entity)
-                print(f'{entity} not generated')
+                print(f'Generating {entity}')
                 generate_model(entity, int(lvl)-1)
-            toadd.append(f'{name}_{entity}:foreignkey:{entity}')
+                generated.append(entity)
+            if entity == schema_name:
+                toadd.append(f'{name}_{entity}:foreignkey:"self"')
+            # Temp fix to avoid reverse accessors
+            elif is_substring_in_list(entity, toadd):
+                toadd.append(f'{name}_{entity}:CharField')
+            else:
+                toadd.append(f'{name}_{entity}:foreignkey:{entity}')
         else:
             toadd.append(f'{name}:{get_mapping(entity)}')
-            print(f'Append {name}')
     gen = Generator('test', schema_name, toadd, False, False)
     gen.run()
 
